3aee46f5a895d6ac5bddd44cbdbfe449d540a502
Kirill Bobeshko <bobeshko@icloud.com>
2022-03-26 14:49:56 +0500

    RandomNumberGenerator.cs created

---
+++ ContinuedFractionsResearcher/ContinuedFractions/RandomNumberGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ContinuedFractionsResearcher
+{
+    public static class RandomNumberGenerator
+    {
+        private static byte[] bytes = new byte[4];
+
+        private static System.Security.Cryptography.RandomNumberGenerator rnd = System.Security.Cryptography
+            .RandomNumberGenerator
+            .Create();
+
+        public static int GetNextInt32()
+        {
+            rnd.GetBytes(bytes);
+            return Math.Abs(BitConverter.ToInt32(bytes, 0));
+        }
+    }
+    
+    
+}
