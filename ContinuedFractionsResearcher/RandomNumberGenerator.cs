147178f444b7c14db6155a56a4e909ca063057ec
Kirill Bobeshko <bobeshko@icloud.com>
2022-03-31 12:47:51 +0500

    epplus added

--- ContinuedFractionsResearcher/RandomNumberGenerator.cs
+++ ContinuedFractionsResearcher/RandomNumberGenerator.cs
@@ -4,18 +4,9 @@ namespace ContinuedFractionsResearcher
 {
     public static class RandomNumberGenerator
     {
-        private static byte[] bytes = new byte[4];
-
-        private static System.Security.Cryptography.RandomNumberGenerator rnd = System.Security.Cryptography
-            .RandomNumberGenerator
-            .Create();
-        // private static Random rnd = new Random();
-
-        public static int GetNextInt16()
+        public static int GetNextInt(int min, int max)
         {
-            // return rnd.Next(1, 1000);
-            rnd.GetBytes(bytes);
-            return Math.Abs(BitConverter.ToInt16(bytes, 0));
+            return System.Security.Cryptography.RandomNumberGenerator.GetInt32(min, max);
         }
     }
 }
